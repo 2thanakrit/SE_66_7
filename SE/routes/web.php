f47b75bcb407da4b891e579532f088c0b8ae57b9
2thanakrit <thanakrit.01224@gmail.com>
2024-03-29 00:21:23 +0700

    edit route

--- SE/routes/web.php
+++ SE/routes/web.php
@@ -26,15 +26,12 @@ use App\Models\User;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
 Route::middleware(['guest'])->group(function () {
 
-Route::get('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/login', [AuthController::class, 'loginPost'])->name('login');
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/login', [AuthController::class, 'loginPost'])->name('login');
 
-//layout
-Route::get('/layoutV2', function () {
-    return view('layoutV2');
-});
 });
 
 Route::get('/', function () {
@@ -45,83 +42,94 @@ Route::get('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/register', [AuthController::class, 'registerPost'])->name('register');
 
 
+
 Route::middleware(['auth'])->group(function () {
 
-Route::get('/home', [HomeController::class, 'index'])->name('home')->middleware('auth');    
-Route::delete('/logout',[AuthController::class, 'logout'])->name('logout');
+
+    Route::middleware(['role:ผู้ดูแลระบบ'])->group(function () {
+        Route::get('/user/create', [UserController::class, 'indexCreate'])->name('createUser');
+        Route::get('/user/edit/{id}', [UserController::class, 'indexEdit'])->name('userEdit');
+        Route::post('/user/create', [UserController::class, 'userStore'])->name('userStore');
+        Route::put('/user/update/{id}', [UserController::class, 'userUpdate'])->name('userUpdate');
+        Route::get('/userMain', [UserController::class, 'index'])->name('userMain');
+        Route::get('/userMain/search', [UserController::class, 'searchUser'])->name('searchUser');
+        Route::delete('/user/delete/{id}', [UserController::class, 'userDelete'])->name('userDelete');
+
+        Route::get('/calendar/create', [CalendarController::class, 'create'])->name('calendar.create');
+        Route::post('/calendar', [CalendarController::class, 'store'])->name('calendar.store');
+        Route::get('calendar/{id}/edit', [CalendarController::class, 'edit'])->name('calendar.edit');
+        Route::put('/calendar/{id}', [CalendarController::class, 'update'])->name('calendar.update');
+        Route::delete('/calendar/{id}', [CalendarController::class, 'destroy'])->name('calendar.destroy');
+
+        Route::get('/display/subcategory', [SubcategoryController::class, 'showAllSubcategory'])->name('showAllSubcategory');
+        Route::get('/add/subcategory', [SubcategoryController::class, 'addSubcategory'])->name('addSubcategory');
+        Route::get('/edit/subcategory', [SubcategoryController::class, 'editSubcategory'])->name('editSubcategory');
+        Route::get('delete/subcategory/{id}', [SubcategoryController::class, 'deleteSubcategory'])->name('deleteSubcategory');
+
+        Route::get('/date_names/create', [DateNameController::class, 'create'])->name('date_names.create');
+        Route::post('/date_names', [DateNameController::class, 'store'])->name('date_names.store');
+        Route::delete('/date_names/{id}', [DateNameController::class, 'destroy'])->name('date_names.destroy');
+        Route::get('date_names/{id}/edit', [DateNameController::class, 'edit'])->name('date_names.edit');
+        Route::put('/date_names/{id}', [DateNameController::class, 'update'])->name('date_names.update');
+
+        Route::get('/display/typeleave', [TypeleaveController::class, 'showAllTypeleave'])->name('showAllTypeleave');
+        Route::get('/edit/typeleave', [TypeleaveController::class, 'editTypeleave'])->name('editTypeleave');
+
+        Route::get('/leavehis', [HisleaveController::class, 'showleavehis']);
+    });
+
+    
+
+    Route::get('/home', [HomeController::class, 'index'])->name('home')->middleware('auth');
+    Route::delete('/logout', [AuthController::class, 'logout'])->name('logout');
+
+    Route::get('/leaveMain', [LeaveOfAbsenseController::class, 'index'])->name('leaveMain');
+    Route::get('/leaveMain/Create', [LeaveOfAbsenseController::class, 'create'])->name('leaveCreate');
+    Route::post('/leaveMain/Create', [LeaveOfAbsenseController::class, 'store'])->name('leaveStore');
+    Route::get('/leaveMain/search', [LeaveOfAbsenseController::class, 'search'])->name('searchLeave');
+    Route::get('/leaveDetail/{id}', [LeaveOfAbsenseController::class, 'detail'])->name('leaveDetail');
+    Route::get('/leaveDetail/{file}', [LeaveOfAbsenseController::class, 'download'])->name('leaveDownload');
+
+    //การเข้างาน
+    Route::get('/Attendance', [AttendanceController::class, 'showAllatten'])->name('display.attendance');
+    Route::get('/Attendance/search', [AttendanceController::class, 'search'])->name('display.search');
+    Route::post('/attendance/add', [AttendanceController::class, 'addAttendance'])->name('attendance.add');
 
 
-Route::get('/user/edit/{id}', [UserController::class, 'indexEdit'])->name('userEdit');
-Route::post('/user/create', [UserController::class, 'userStore'])->name('userStore');
-Route::put('/user/update/{id}', [UserController::class, 'userUpdate'])->name('userUpdate');
-Route::get('/userMain', [UserController::class, 'index'])->name('userMain');
-Route::get('/userMain/search', [UserController::class, 'searchUser'])->name('searchUser');
-Route::delete('/user/delete/{id}', [UserController::class, 'userDelete'])->name('userDelete');
+    Route::middleware(['auth', 'role:หัวหน้าหมวด,ผู้อำนวยการ,รองผู้อำนวยการ'])->group(function () {
+        Route::get('/display/leaveofabsences', [CrudController::class, 'showAllLeaveofabsences']);
+        Route::get('/edit/leaveofabsences', [CrudController::class, 'editLeave'])->name('editLeave');
+        Route::get('/leaveofabsences/search', [CrudController::class, 'search'])->name('searchLeaveHis');
+        Route::get('/add/leaveofabsences', [CrudController::class, 'addLeave'])->name('addLeave');
+        Route::get('/detailLeave/{id}', [CrudController::class, 'detail'])->name('detailLeave');
+        Route::get('/searchapprover', [CrudController::class, 'search']);
 
-Route::get('/leaveMain', [LeaveOfAbsenseController::class, 'index'])->name('leaveMain');
-Route::get('/leaveMain/Create', [LeaveOfAbsenseController::class, 'create'])->name('leaveCreate');
-Route::post('/leaveMain/Create', [LeaveOfAbsenseController::class, 'store'])->name('leaveStore');
-Route::get('/leaveMain/search', [LeaveOfAbsenseController::class, 'search'])->name('searchLeave');
-Route::get('/leaveDetail/{id}', [LeaveOfAbsenseController::class, 'detail'])->name('leaveDetail');
-Route::get('/leaveDetail/{file}', [LeaveOfAbsenseController::class, 'download'])->name('leaveDownload');
+        Route::get('/acknowledge', [AcknowledgeController::class, 'index'])->name('ackindex');
+        Route::get('/search', [AcknowledgeController::class, 'search']);
+        Route::get('/acknowledge/{id}', [AcknowledgeController::class, 'acknowledge'])->name('accept');
+        Route::get('/acknowledge/detail/{id}', [AcknowledgeController::class, 'show'])->name('ackdetail');
+        Route::get('/acknowledge/detail/{file}', [AcknowledgeController::class, 'download'])->name('ackdownload');
 
-Route::get('/acknowledge', [AcknowledgeController::class, 'index'])->name('ackindex');
-Route::get('/search', [AcknowledgeController::class,'search']);
-Route::get('/acknowledge/{id}', [AcknowledgeController::class,'acknowledge'])->name('accept');
-Route::get('/acknowledge/detail/{id}', [AcknowledgeController::class,'show'])->name('ackdetail');
-Route::get('/acknowledge/detail/{file}', [AcknowledgeController::class,'download'])->name('ackdownload');
+    });
+    
+    // Route::get('/leavetype', function () {
+    //     return view('leavetype');
+    // });
 
-//การเข้างาน
-Route::get('/Attendance', [AttendanceController::class, 'showAllatten'])->name('display.attendance');
-Route::get('/Attendance/search', [AttendanceController::class,'search'])->name('display.search');
-Route::post('/attendance/add', [AttendanceController::class, 'addAttendance'])->name('attendance.add');
-
-Route::get('/display/leaveofabsences',[CrudController::class,'showAllLeaveofabsences']);
-Route::get('/edit/leaveofabsences',[CrudController::class,'editLeave'])->name('editLeave');
-Route::get('/leaveofabsences/search', [CrudController::class, 'search'])->name('searchLeaveHis');
-Route::get('/add/leaveofabsences',[CrudController::class,'addLeave'])->name('addLeave');
-Route::get('/detailLeave/{id}', [CrudController::class, 'detail'])->name('detailLeave');
-Route::get('/searchapprover', [CrudController::class,'search']);
-
-Route::get('/leavetype', function () {return view('leavetype');});
-Route::get('/leavehis', function () {return view('leavehis');});
-Route::get('/leavetype', [HisleaveController::class, 'showleavetype']);
-Route::get('/leavehis', [HisleaveController::class, 'showleavehis']);
-
-Route::get('/display/subcategory',[SubcategoryController::class,'showAllSubcategory'])->name('showAllSubcategory');
-Route::get('/add/subcategory',[SubcategoryController::class,'addSubcategory'])->name('addSubcategory');
-Route::get('/edit/subcategory',[SubcategoryController::class,'editSubcategory'])->name('editSubcategory');
-Route::get('delete/subcategory/{id}',[SubcategoryController::class,'deleteSubcategory'])->name('deleteSubcategory');
-//หน้าจัดการชนิดการลาแก้ไขชื่อ-จำนวน
-Route::get('/display/typeleave',[TypeleaveController::class,'showAllTypeleave'])->name('showAllTypeleave');
-Route::get('/edit/typeleave',[TypeleaveController::class,'editTypeleave'])->name('editTypeleave');
+    // Route::get('/leavehis', function () {
+    //     return view('leavehis');
+    // });
 
 
-Route::middleware(['auth', 'role:ผู้ดูแลระบบ'])->group(function () {
-    // Your routes here
-    Route::get('/user/create', [UserController::class, 'indexCreate'])->name('createUser');
-});
-Route::get('/calendar', [CalendarController::class, 'index'])->name('calendar.index');
-Route::delete('/calendar/{id}',[CalendarController::class, 'destroy'])->name('calendar.destroy');
+    Route::get('/leavetype', [HisleaveController::class, 'showleavetype']);
 
-// เส้นทางสำหรับแสดงหน้าสร้างข้อมูลใหม่ของ Calendar
-Route::get('/calendar/create', [CalendarController::class, 'create'])->name('calendar.create');
-
-// เส้นทางสำหรับบันทึกข้อมูลใหม่ของ Calendar
-Route::post('/calendar', [CalendarController::class, 'store'])->name('calendar.store');
-// เส้นทางสำหรับแก้ไขข้อมูลใหม่ของ Calendar
-Route::get('calendar/{id}/edit', [CalendarController::class, 'edit'])->name('calendar.edit');
-Route::put('/calendar/{id}', [CalendarController::class, 'update'])->name('calendar.update');
-Route::get('/calendar/{id}', [CalendarController::class, 'detail'])->name('calendar.detail');
+    //หน้าจัดการชนิดการลาแก้ไขชื่อ-จำนวน
 
 
-//DateName
-Route::get('/date_names', [DateNameController::class, 'index'])->name('date_names.index');
-Route::get('/date_names/create', [DateNameController::class, 'create'])->name('date_names.create');
-Route::post('/date_names', [DateNameController::class, 'store'])->name('date_names.store');
-Route::delete('/date_names/{id}',[DateNameController::class, 'destroy'])->name('date_names.destroy');
-Route::get('date_names/{id}/edit', [DateNameController::class, 'edit'])->name('date_names.edit');
-Route::put('/date_names/{id}', [DateNameController::class, 'update'])->name('date_names.update');
+    //Calendar  
+    Route::get('/calendar', [CalendarController::class, 'index'])->name('calendar.index');
+    Route::get('/calendar/{id}', [CalendarController::class, 'detail'])->name('calendar.detail');
 
-
+    //DateName
+    Route::get('/date_names', [DateNameController::class, 'index'])->name('date_names.index');
 });
