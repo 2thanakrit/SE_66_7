db7728ddbde6b56037245e0413e1e295e1c501fa
dimonkub <156262013+dimonkub@users.noreply.github.com>
2024-03-29 00:27:44 +0700

    แก้ไม่ให้เพิ้มข้อมูลซ้ำ

--- SE/app/Models/Calendar.php
+++ SE/app/Models/Calendar.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Validation\Rule;
 
 class Calendar extends Model
 {
@@ -21,4 +22,18 @@ class Calendar extends Model
     protected $fillable = [
         'date', 'dateN_id', 'checkRest', 'detail',
     ];
+    public static function boot()
+    {
+        parent::boot();
+
+        static::saving(function ($model) {
+            // ตรวจสอบว่าชื่อวันที่ซ้ำกันหรือไม่
+            $duplicate = Calendar::where('date', $model->date)->exists();
+
+            // หากมีชื่อวันที่ซ้ำกันในฐานข้อมูล ให้ยกเลิกการบันทึก
+            if ($duplicate) {
+                return false;
+            }
+        });
+    }
 }
--- SE/app/Models/datename.php
+++ SE/app/Models/datename.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Validation\Rule;
 
 class datename extends Model
 {
@@ -18,4 +19,20 @@ class datename extends Model
 {
     return $this->hasMany(Calendar::class, 'dateN_id', 'id');
 }
+public static function boot()
+    {
+        parent::boot();
+
+        // กำหนดเหตุการณ์ก่อนการบันทึกข้อมูล
+        static::saving(function ($datename) {
+            // ตรวจสอบว่าชื่อวันที่มีอยู่ในฐานข้อมูลแล้วหรือไม่
+            $existingDate = datename::where('name', $datename->name)->exists();
+
+            // ถ้ามีชื่อวันที่ซ้ำอยู่ในฐานข้อมูล
+            if ($existingDate) {
+                // ส่งกลับค่า false เพื่อยกเลิกการบันทึกข้อมูล
+                return false;
+            }
+        });
+    }
 }
