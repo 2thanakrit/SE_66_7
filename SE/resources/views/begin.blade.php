c067f9d9ac28a5859259f5c39a765a27f5e190cb
KhunSanprom <khun.sanprom@gmail.com>
2024-03-28 21:41:51 +0700

    แก้ไขหน้าเริ่มต้นหลังจาก login
    - create begin view
    - add image
    - css begin
    - fix homecontroller --> จาร layout เป้น begin

---
+++ SE/resources/views/begin.blade.php
@@ -0,0 +1,29 @@
+@extends('layout')
+
+@section('content')
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('assets/css/begin.css') }}"> 
+</head>
+
+<body>
+    <div class="begin">
+        <h1> สมาชิกกลุ่มที่ 7 </h1>
+        <h3>6420500931 ขุนพล แสนพรหม </h3>
+        <h3>6420502098 ธนกฤต เสถียรบัวงาม</h3>
+        <h3>6420503698 ศรคม คุ้มคำ</h3>
+        <h3>6420503655 เพชรกล้า ประดิษฐ์มณีโชติ </h3>
+        <h3>6420503671 วงศกร บุญชัยศรี</h3>
+        <h3>6420502101 ธันยชนก นาคอร่าม</h3>
+        <h3>6420502136 พนธ์กฤษณ์ จินดานุ</h3>
+       
+    </div>
+</body>
+
+</html>
+@endsection
